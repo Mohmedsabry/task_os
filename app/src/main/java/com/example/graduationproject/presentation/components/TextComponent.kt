525cee1cbd2e1bd861cff66f2661a67b546b41d2
mohmedsabry <mayjak60@gmail.com>
2023-08-22 23:55:54 +0300

    components

--- app/src/main/java/com/example/graduationproject/presentation/components/TextComponent.kt
+++ app/src/main/java/com/example/graduationproject/presentation/components/TextComponent.kt
@@ -6,27 +6,35 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import com.example.graduationproject.R
 
-@RequiresApi(Build.VERSION_CODES.Q)
+
 @Composable
 fun TextShow(
     text: String,
-    color: Long = Color.White.value.toLong(),
-    fontSize: Int = 300,
-    weight: Int = 600
+    color: Color,
+    fontSize: Int = 22,
+    weight: Int = 600,
+    fontFamily: FontFamily
 ) {
     Text(
         text = text,
         style = TextStyle(
             fontSize = fontSize.sp,
-            fontFamily = FontFamily(Font(R.font.open)),
+            fontFamily = fontFamily,
             fontWeight = FontWeight(weight),
-            color = Color(color),
+            color = color,
         )
     )
+}
+
+@RequiresApi(Build.VERSION_CODES.Q)
+@Preview(showBackground = true)
+@Composable
+fun preview() {
+
+
 }
