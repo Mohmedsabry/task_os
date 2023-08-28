032a8cbd647993015e9ffc7135f1dbedb248b0ce
nourasaad123 <nourasaadibrahim836@gmail.com>
2023-08-28 16:10:13 +0300

    favourite card

--- app/src/main/java/com/example/graduationproject/presentation/activites/MainActivity.kt
+++ app/src/main/java/com/example/graduationproject/presentation/activites/MainActivity.kt
@@ -20,11 +20,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -34,12 +33,15 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewModelScope
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
@@ -106,9 +108,11 @@ class MainActivity : ComponentActivity() {
                 var showBottomSheet by remember {
                     mutableStateOf(false)
                 }
-                Box(modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Red)) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Red)
+                ) {
 
                     LazyColumn(
                         modifier = Modifier
@@ -256,7 +260,7 @@ class MainActivity : ComponentActivity() {
                 Card(
                     shape = CardDefaults.outlinedShape,
                     colors = CardDefaults.cardColors(CustomColor.lightGray),
-                    modifier = Modifier
+                    modifier = Modifier.background(CustomColor.lightGray,shape = RoundedCornerShape(size = 20.dp))
                         .padding(10.dp)
                         .align(Alignment.Start)
                 ) {
@@ -303,48 +307,56 @@ class MainActivity : ComponentActivity() {
                                         fontFamily = FontFamily.Default,
                                         fontSize = 13
                                     )
-                                    TextShow(
-                                        text = list2[index].currency,
-                                        color = CustomColor.black,
-                                        fontFamily = FontFamily.Default,
-                                        fontSize = 11
+                                    Text(
+                                        text = "CURRENCY",
+                                        style = TextStyle(
+                                            fontSize = 11.56.sp,
+                                            lineHeight = 19.27.sp,
+                                            fontFamily = FontFamily(Font(R.font.poppins)),
+                                            fontWeight = FontWeight(400),
+                                            color = Color(0xFFB8B8B8),
+                                        )
                                     )
                                 }
 
                                 Spacer(modifier = Modifier.weight(1f))
 
-                                    Box(modifier = Modifier
-                                        .clickable {
-                                            isCheck = !isCheck
-                                        }
-                                        .size(25.dp),
-                                        contentAlignment = Alignment.Center
-                                    ) {
-                                        if (isCheck) {
-                                            Icon(modifier=Modifier.size(30.dp),
-                                                painter = painterResource(id = R.drawable.grop),
-                                                contentDescription = "checked"
-                                            )
-                                        }else{
-                                            Icon(modifier=Modifier.size(30.dp),
-                                                painter = painterResource(id = R.drawable.ellipse), contentDescription ="checked" )
-
-                                        }
+                                Box(modifier = Modifier
+                                    .clickable {
+                                        isCheck = !isCheck
                                     }
+                                    .size(25.dp),
+                                    contentAlignment = Alignment.Center
+                                ) {
                                     if (isCheck) {
-                                        coroutineScope.launch(Dispatchers.IO) {
-                                            repository.insertRoom(list2[index])
-                                            println("${repository.getAllFav().size} insert")
-                                            viewModel.updateFlow()
-                                        }
+                                        Icon(
+                                            modifier = Modifier.size(30.dp),
+                                            painter = painterResource(id = R.drawable.grop),
+                                            contentDescription = "checked"
+                                        )
                                     } else {
-                                        coroutineScope.launch(Dispatchers.IO) {
-                                            repository.deleteRoom(list2[index])
-                                            println("${repository.getAllFav().size} delete")
-                                            viewModel.updateFlow()
-                                        }
+                                        Icon(
+                                            modifier = Modifier.size(30.dp),
+                                            painter = painterResource(id = R.drawable.ellipse),
+                                            contentDescription = "checked"
+                                        )
+
                                     }
                                 }
+                                if (isCheck) {
+                                    coroutineScope.launch(Dispatchers.IO) {
+                                        repository.insertRoom(list2[index])
+                                        println("${repository.getAllFav().size} insert")
+                                        viewModel.updateFlow()
+                                    }
+                                } else {
+                                    coroutineScope.launch(Dispatchers.IO) {
+                                        repository.deleteRoom(list2[index])
+                                        println("${repository.getAllFav().size} delete")
+                                        viewModel.updateFlow()
+                                    }
+                                }
+                            }
 
                         }
                     }
